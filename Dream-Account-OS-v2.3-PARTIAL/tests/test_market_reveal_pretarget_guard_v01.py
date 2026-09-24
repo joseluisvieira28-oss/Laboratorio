f732ae5b16b09f27ea1018db1e490ac239d0a191
joseluisvieira28-oss <joseluisvieira28@gmail.com>
2026-09-24 19:40:00 +0200

    MRCR V0.1: extend anti-hindsight guard to reconstruction engine

--- Dream-Account-OS-v2.3-PARTIAL/tests/test_market_reveal_pretarget_guard_v01.py
+++ Dream-Account-OS-v2.3-PARTIAL/tests/test_market_reveal_pretarget_guard_v01.py
@@ -8,6 +8,9 @@ MODULE_DIR = ROOT / "research" / "market_reveal_confirmation_reaction_v01"
 CODE_FILES = [
     MODULE_DIR / "measurements.py",
     MODULE_DIR / "source_adapters.py",
+    MODULE_DIR / "decision_boundary.py",
+    MODULE_DIR / "order_book.py",
+    MODULE_DIR / "state_reconstruction.py",
 ]
 
 FORBIDDEN_IDENTIFIERS = {
